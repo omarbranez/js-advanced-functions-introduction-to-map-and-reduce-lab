611ab60d4cba8b7c7f287d84d34e018b9e223344
Omar <omar.branez@gmail.com>
2021-08-19 00:07:40 -0400

    commit tree bac1f5dd7beee70a56ec89ce85c3ce4c9c27a9b3

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for (const el of sourceArray) {
+        newArray.push(el * -1)
+    }
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = []
+    for (const el of sourceArray) {
+        newArray.push(el)
+    }
+    return newArray
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = []
+    for (const el of sourceArray) {
+        newArray.push(el * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = []
+    for (const el of sourceArray) {
+        newArray.push(el ** 2)
+    }
+    return newArray
+}
+
+function reduceToTotal(sourceArray, startingPoint) {
+    let reducer = (accumulator, currentValue) => accumulator + currentValue
+    if (startingPoint) {
+       return sourceArray.reduce(reducer, startingPoint)
+    } else {
+       return sourceArray.reduce(reducer)
+    }    
+}
+
+function reduceToAllTrue(sourceArray) {
+    let el = sourceArray.values()
+    if (sourceArray.includes(!el)) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function reduceToAnyTrue(sourceArray) {
+    if (sourceArray.find(el => el)) {
+        return true
+    } else {
+    return false
+    }
+}
